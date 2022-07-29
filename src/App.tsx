2f0907bbf17ae892ce094c5a19f111246e132184
ujjwalsingh108 <usingh2051@gmail.com>
2022-07-29 16:05:12 +0530

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  ReadyPage,
+  ErrorComponent,
+  LoginPage,
+} from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+import "styles/antd.less";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import { authProvider } from "./authProvider";
+
+function App() {
+  return (
+    <Refine
+      notificationProvider={notificationProvider}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      authProvider={authProvider}
+      LoginPage={LoginPage}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+    />
+  );
+}
+
+export default App;
